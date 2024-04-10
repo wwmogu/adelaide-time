efbd794b469c996d69d0194cedea155efe24aabf
wwmogu <wwmogu@gmail.com>
2024-04-10 20:26:40 +0930

    Can convert new york time to adelaide time now.

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-// function getCountryName() {
-//     return document.getElementById("countryName").value;
-// }
+function getCountryName() {
+    return document.getElementById("countryName").value;
+}
 
-// function getCountryTime() {
-//     const countryTime = document.getElementById("countryTime").value;
-//     return countryTime;
-// }
+function getCountryTime() {
+    const countryTime = document.getElementById("countryTime").value;
+    return countryTime;
+}
 
-function convertTime() {
-    var newYork    = moment.tz("2014-06-01 12:00", "America/New_York");
-    var losAngeles = newYork.clone().tz("America/Los_Angeles");
-    var london     = newYork.clone().tz("Europe/London");
+function est2acst() {
+    // new york is in est time zone
+    // adelaide is in acst time zone
+    var userTime = getCountryTime();
+    console.log("User Time: " + userTime);
+    var estTime = moment.tz(userTime, "America/New_York");
+    var adelaideTime = estTime.clone().tz("Australia/Adelaide");
+    console.log("EST Time: " + estTime.format());
+    console.log("Adelaide Time: " + adelaideTime.format());
 
-    console.log(newYork.format()    );// 2014-06-01T12:00:00-04:00
-    console.log(losAngeles.format() );// 2014-06-01T09:00:00-07:00
-    console.log(london.format()     );// 2014-06-01T17:00:00+01:00
+    // var newYork    = moment.tz("2014-06-01 12:00", "America/New_York");
+    // var losAngeles = newYork.clone().tz("America/Los_Angeles");
+    // var london     = newYork.clone().tz("Europe/London");
+
+    // console.log(newYork.format()    );// 2014-06-01T12:00:00-04:00
+    // console.log(losAngeles.format() );// 2014-06-01T09:00:00-07:00
+    // console.log(london.format()     );// 2014-06-01T17:00:00+01:00
 }
